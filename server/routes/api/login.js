0258cad737e27a201a31c2e48a83d3aab5e0aeff
magda <magdapolak96@gmail.com>
2018-07-24 23:17:00 +0200

    Basic schema of backend JWT handling

---
+++ server/routes/api/login.js
@@ -0,0 +1,41 @@
+const User = require('../../models/Admin');
+const JWTtoken = require('../../libs/auth');
+//import { paramCheck } from '../middlewares'
+
+//const router = express.Router()
+
+/*
+  ROUTES
+*/
+
+//router.post('*', paramCheck(['email', 'password']))
+
+module.exports = (app) => {
+  app.post('/api/login', (req, res) => {
+    let {email, password} = req.body
+
+    User.findById(email)
+      .exec()
+      .then((user) => (!user) ? Promise.reject("User not found.") : user)
+      .then((user) => user.comparePassword(password))
+      //.then((user) => user.publicParse(user))
+      .then((user) => {
+        res.status(200)
+          .json({
+            success: true,
+            token: JWTtoken.createJWToken({
+              sessionData: user,
+              maxAge: 3600
+            })
+          })
+      })
+      .catch((err) => {
+        res.status(401)
+          .json({
+            message: err || "Validation failed. Given email and password aren't matching."
+          })
+      })
+  })
+}
+
+//export default router
