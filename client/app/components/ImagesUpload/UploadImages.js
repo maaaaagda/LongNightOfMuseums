d1613b161c2aa469222076eb982df647d4f7c44d
magda <magdapolak96@gmail.com>
2018-09-07 22:16:01 +0200

    Background photo is now versioned

--- client/app/components/ImagesUpload/UploadImages.js
+++ client/app/components/ImagesUpload/UploadImages.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import '../../styles/photo_upload.less';
-import UploadIcon from './../../assets/UploadIcon.svg';
+import UploadIcon from '../../assets/upload-icon.svg';
 import {Icon} from "semantic-ui-react";
 
 class ReactImageUploadComponent extends React.Component {
